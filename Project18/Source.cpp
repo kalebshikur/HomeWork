45b589b3716c7699ec2d694a25f1fa5cd2bf8b3d
kalebshikur <shikurkm@mymail.vcu.edu>
2018-10-31 21:14:05 -0400

    commented

--- Project18/Source.cpp
+++ Project18/Source.cpp
@@ -4,19 +4,22 @@ using namespace std;
 void reversePhrase(char phrase[100]);
 bool checker(char phrase[100]);
 int main() {
+	// store the string to be tested
 	char phrase[100];
 	cout << "Enter the phrases: " << endl;
 	cin.getline(phrase, 100);
+	// check the value of the checker function and display the apropriate string
 	if (checker(phrase) == 1)
 	{
 		cout << "The phrase is palindrome" << endl;
 	}
 	else
 	cout << "The phrase is not palindrome" << endl;
+	// display the reversed string
 	reversePhrase(phrase);
 	system("pause");
 }
-
+// The function reverses the string and display the reversed version
 void reversePhrase(char phrase[100]) {
 	int i, j;
 	char temp;
@@ -29,7 +32,7 @@ void reversePhrase(char phrase[100]) {
 	}
 	cout << phrase << endl;
 }
-
+// check weather the phrase is a palindrome and return a bool value
 bool checker(char phrase[100]) {
 	bool test = true;
 	int i, j;
